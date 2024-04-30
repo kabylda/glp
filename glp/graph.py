e67e7a949a4ff2f2c175c7aecca19fffc3092116
suadou <sergio.suarezdou@uni.lu>
2024-04-30 15:27:31 +0200

    Including electrostatics cutoff

--- glp/graph.py
+++ glp/graph.py
@@ -7,20 +7,22 @@ import jax.numpy as jnp
 from .utils import cast
 from .periodic import displacement
 
-Graph = namedtuple("Graph", ("edges", "nodes", "centers", "others", "mask"))
+Graph = namedtuple("Graph", ("positions", "edges", "nodes", "centers", "others", "mask", "total_charge", "num_unpaired_electrons", "idx_i_lr", "idx_j_lr",  "pair_displacements", "cell", "ngrid", "alpha", "frequency"))
 
-
-def system_to_graph(system, neighbors):
+def system_to_graph(system, neighbors, pme):
     # neighbors are an *updated* neighborlist
     # question: how do we treat batching?
 
     positions = system.R
     nodes = system.Z
-
     edges = jax.vmap(partial(displacement, system.cell))(
+        positions[neighbors.centers], positions[neighbors.others]
+    )
+    pair_displacements = jax.vmap(partial(displacement, system.cell))(
         positions[neighbors.centers], positions[neighbors.others]
     )
 
     mask = neighbors.centers != positions.shape[0]
 
-    return Graph(edges, nodes, neighbors.centers, neighbors.others, mask)
+    return Graph(positions, edges, nodes, neighbors.centers, neighbors.others, mask, system.total_charge, system.num_unpaired_electrons, neighbors.pair_i, neighbors.pair_j, pair_displacements, system.cell,  pme.ngrid, pme.alpha, pme.frequencies)
+
