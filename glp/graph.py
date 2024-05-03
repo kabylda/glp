846d023fe5fcb7790abbe7d831fd3fadc214e81b
suadou <sergio.suarezdou@uni.lu>
2024-05-03 20:05:14 +0200

    Update to work with MLFF & PME

--- glp/graph.py
+++ glp/graph.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 from .utils import cast
 from .periodic import displacement
 
-Graph = namedtuple("Graph", ("positions", "edges", "nodes", "centers", "others", "mask", "total_charge", "num_unpaired_electrons", "idx_i_lr", "idx_j_lr",  "d_ij_lr", "cell", "ngrid", "alpha", "frequency"))
+Graph = namedtuple("Graph", ("positions","edges", "nodes", "centers", "others", "mask", "total_charge", "num_unpaired_electrons", "edges_lr", "idx_i_lr", "idx_j_lr", "cell", "ngrid", "alpha", "frequency"))
 
 def system_to_graph(system, neighbors, pme):
     # neighbors are an *updated* neighborlist
@@ -15,14 +15,18 @@ def system_to_graph(system, neighbors, pme):
 
     positions = system.R
     nodes = system.Z
+
     edges = jax.vmap(partial(displacement, system.cell))(
-        positions[neighbors.centers], positions[neighbors.others]
-    )
-    d_ij_lr = jax.vmap(partial(displacement, system.cell))(
-        positions[neighbors.idx_i_lr], positions[neighbors.idx_j_lr]
-    )
+        positions[neighbors.centers], positions[neighbors.others])
+    edges_lr = jax.vmap(partial(displacement, system.cell))(
+        positions[neighbors.idx_i_lr], positions[neighbors.idx_j_lr])
 
     mask = neighbors.centers != positions.shape[0]
 
-    return Graph(positions, edges, nodes, neighbors.centers, neighbors.others, mask, system.total_charge, system.num_unpaired_electrons, neighbors.idx_i_lr, neighbors.idx_j_lr, d_ij_lr, system.cell,  pme.ngrid, pme.alpha, pme.frequencies)
+    if not pme:
+        ngrid = alpha = frequencies = None
+    else:
+        ngrid, alpha, frequencies = pme.ngrid, pme.alpha, pme.frequencies
+
+    return Graph(positions, edges, nodes, neighbors.centers, neighbors.others, mask, system.total_charge, system.num_unpaired_electrons, edges_lr, neighbors.idx_i_lr, neighbors.idx_j_lr, system.cell, ngrid, alpha, frequencies)
 
